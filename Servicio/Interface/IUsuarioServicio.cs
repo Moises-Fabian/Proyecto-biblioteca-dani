9fb794d99019eba410460cb91c1f2a14559b1e51
Moises-Fabian <76923722+Moises-Fabian@users.noreply.github.com>
2025-09-25 19:30:27 -0300

    se renombran funciones

--- Servicio/Interface/IUsuarioServicio.cs
+++ Servicio/Interface/IUsuarioServicio.cs
@@ -1,18 +1,13 @@
 ﻿using BookWorldApp.Modelo.Entidades;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BookWorldApp.Servicio.Interface
 {
     public interface IUsuarioServicio
     {
         List<Usuario> ObtenerTodos();
-        void Agregar(Usuario usuario);
-        void Editar(Usuario usuarioActualizado);
-        void Eliminar(string rut);
-        bool Existe(string rut);
+        void AgregarUsuario(Usuario usuario);
+        void EditarUsuario(Usuario usuarioActualizado);
+        void EliminarUsuario(string rut);
+        bool ExisteUsuario(string rut);
     }
 }
