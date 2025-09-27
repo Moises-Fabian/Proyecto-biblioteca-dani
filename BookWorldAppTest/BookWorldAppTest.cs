7ab3270015ab15ee6ad3af4ee3015851e1303078
Moises-Fabian <76923722+Moises-Fabian@users.noreply.github.com>
2025-09-26 21:49:37 -0300

    Se agregan pruebas unitarias

---
+++ BookWorldAppTest/BookWorldAppTest.cs
@@ -0,0 +1,56 @@
+﻿using BookWorldApp.Modelo.Entidades;
+using BookWorldApp.Servicio;
+using NUnit.Framework;
+using System.Linq;
+
+namespace BookWorldAppTest
+{
+    [TestFixture]
+    public class LibroServicioTests
+    {
+        private LibroServicio _libroServicio;
+
+        [SetUp]
+        public void Setup()
+        {
+            _libroServicio = new LibroServicio();
+        }
+
+        [Test]
+        public void AgregarLibro_CuandoLibroEsValido_AnadirloALaLista()
+        {
+            var nuevoLibro = new Libro { Id = 4, Titulo = "Nuevo Libro", Autor = "Nuevo Autor", Existencias = 10, AnioPublicacion = 2025 };
+
+            _libroServicio.AgregarLibro(nuevoLibro);
+
+            var libros = _libroServicio.ObtenerLibros();
+            Assert.That(libros.Count, Is.EqualTo(4));
+            Assert.That(libros.Any(l => l.Id == 4), Is.True);
+        }
+
+        [Test]
+        public void EditarLibro_CuandoLibroExiste_DeberiaActualizarSusDatos()
+        {
+            var libroActualizado = new Libro { Id = 2, Titulo = "Título Editado", Autor = "Autor Editado", Existencias = 99 };
+
+            _libroServicio.EditarLibro(libroActualizado);
+
+            var libro = _libroServicio.ObtenerLibros().First(l => l.Id == 2);
+            Assert.That(libro.Titulo, Is.EqualTo("Título Editado"));
+            Assert.That(libro.Existencias, Is.EqualTo(99));
+        }
+
+        [Test]
+        public void EliminarLibro_CuandoLibroExiste_DeberiaQuitarloDeLaLista()
+        {
+            var idAEliminar = 3;
+            var totalInicial = _libroServicio.ObtenerLibros().Count;
+
+            _libroServicio.EliminarLibro(idAEliminar);
+
+            var libros = _libroServicio.ObtenerLibros();
+            Assert.That(libros.Count, Is.EqualTo(totalInicial - 1));
+            Assert.That(libros.Any(l => l.Id == idAEliminar), Is.False);
+        }
+    }
+}
---
+++ BookWorldAppTest/UsuarioServicioTests.cs
@@ -0,0 +1,57 @@
+﻿using BookWorldApp.Modelo.Entidades;
+using BookWorldApp.Servicio;
+using NUnit.Framework;
+using System.Linq;
+
+namespace BookWorldAppTest
+{
+    [TestFixture]
+    public class UsuarioServicioTests
+    {
+        private UsuarioServicio _usuarioServicio;
+
+        [SetUp]
+        public void Setup()
+        {
+            _usuarioServicio = new UsuarioServicio();
+        }
+
+        [Test]
+        public void AgregarUsuario_AnadirloALaLista()
+        {
+            var nuevoUsuario = new Usuario { Rut = "12345678-9", Nombre = "Usuario Nuevo" };
+
+            _usuarioServicio.AgregarUsuario(nuevoUsuario);
+
+            var usuarios = _usuarioServicio.ObtenerUsuarios();
+            Assert.That(usuarios.Count, Is.EqualTo(4));
+            Assert.That(usuarios.Any(u => u.Rut == "12345678-9"), Is.True);
+        }
+
+        [Test]
+        public void EditarUsuariUsuarioExiste_ActualizarSusDatos()
+        {
+            var rutExistente = "1-9";
+            var usuarioActualizado = new Usuario { Rut = rutExistente, Nombre = "Nombre Editado", Correo = "editado@correo.com", Telefono = "12345" };
+
+            _usuarioServicio.EditarUsuario(usuarioActualizado);
+
+            var usuario = _usuarioServicio.ObtenerUsuarios().First(u => u.Rut == rutExistente);
+            Assert.That(usuario.Nombre, Is.EqualTo("Nombre Editado"));
+            Assert.That(usuario.Correo, Is.EqualTo("editado@correo.com"));
+        }
+
+        [Test]
+        public void EliminarUsuarioExiste_QuitarloDeLaLista()
+        {
+            var rutAEliminar = "11111111-1";
+            var totalInicial = _usuarioServicio.ObtenerUsuarios().Count;
+
+            _usuarioServicio.EliminarUsuario(rutAEliminar);
+
+            var usuarios = _usuarioServicio.ObtenerUsuarios();
+            Assert.That(usuarios.Count, Is.EqualTo(totalInicial - 1));
+            Assert.That(usuarios.Any(u => u.Rut == rutAEliminar), Is.False);
+        }
+    }
+}
